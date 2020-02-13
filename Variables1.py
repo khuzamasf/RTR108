a0c4a5539eaf8d02f892d92606e8039d8022d42f
user <khuzamaywbf@hotmail.com>
2020-02-13 09:59:30 +0200

    20200213_09_52

---
+++ Variables1.py
@@ -0,0 +1,75 @@
+Python 3.6.8 (default, Jan 14 2019, 11:02:34) 
+[GCC 8.0.1 20180414 (experimental) [trunk revision 259383]] on linux
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> type(1,000,000)
+Traceback (most recent call last):
+  File "<pyshell#0>", line 1, in <module>
+    type(1,000,000)
+TypeError: type.__new__() argument 1 must be str, not int
+>>> i
+Traceback (most recent call last):
+  File "<pyshell#1>", line 1, in <module>
+    i
+NameError: name 'i' is not defined
+i
+>>> ```
+SyntaxError: invalid syntax
+>>> print(1,000,000)
+1 0 0
+>>> type(3.2)
+<class 'float'>
+>>> message = 'And now for something completely different'
+>>> n = 17
+>>> pi = 3.142
+>>> print(n)
+17
+>>> print(message)
+And now for something completely different
+>>> print(pi)
+3.142
+>>> type(message)
+<class 'str'>
+>>> type(n)
+<class 'int'>
+>>> type(pi)
+<class 'float'>
+>>> Class = 12
+>>> print(Class)
+12
+>>> okay
+Traceback (most recent call last):
+  File "<pyshell#16>", line 1, in <module>
+    okay
+NameError: name 'okay' is not defined
+>>> i
+Traceback (most recent call last):
+  File "<pyshell#17>", line 1, in <module>
+    i
+NameError: name 'i' is not defined
+i
+>>> class = 12
+SyntaxError: invalid syntax
+>>> print(print)
+<built-in function print>
+>>> 
+>>> 
+>>> 5
+5
+>>> x = 5
+>>> x + 1
+6
+>>> 
+>>> i
+Traceback (most recent call last):
+  File "<pyshell#26>", line 1, in <module>
+    i
+NameError: name 'i' is not defined
+>>> print(x)
+5
+>>> x ==5
+True
+>>> x ==6
+False
+>>> x
+5
+>>> 
