551ef87f260282a241a054f6c5dbae444041d441
user <khuzamaywbf@hotmail.com>
2020-02-20 09:53:54 +0200

    20200220

---
+++ Conditional Execution.py
@@ -0,0 +1,105 @@
+Python 3.6.8 (default, Jan 14 2019, 11:02:34) 
+[GCC 8.0.1 20180414 (experimental) [trunk revision 259383]] on linux
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> >>> type(32)
+<class 'int'>
+SyntaxError: invalid syntax
+>>> type(32)
+<class 'int'>
+>>> 
+==================== RESTART: /home/user/RTR108/test1.py ====================
+>>> 
+==================== RESTART: /home/user/RTR108/test1.py ====================
+>>> run
+Traceback (most recent call last):
+  File "<pyshell#2>", line 1, in <module>
+    run
+NameError: name 'run' is not defined
+>>> 22
+22
+>>> 
+==================== RESTART: /home/user/RTR108/test1.py ====================
+22
+>>> 22
+22
+>>> print(45)
+45
+>>> print('41')
+41
+>>> print(x)
+Traceback (most recent call last):
+  File "<pyshell#8>", line 1, in <module>
+    print(x)
+NameError: name 'x' is not defined
+>>> print(x)
+Traceback (most recent call last):
+  File "<pyshell#9>", line 1, in <module>
+    print(x)
+NameError: name 'x' is not defined
+>>> 
+==================== RESTART: /home/user/RTR108/test1.py ====================
+Traceback (most recent call last):
+  File "/home/user/RTR108/test1.py", line 2, in <module>
+    a == 23
+NameError: name 'a' is not defined
+>>> x = 2
+>>> print(x)
+2
+>>> 
+==================== RESTART: /home/user/RTR108/test1.py ====================
+Traceback (most recent call last):
+  File "/home/user/RTR108/test1.py", line 2, in <module>
+    a == 23
+NameError: name 'a' is not defined
+>>> 
+==================== RESTART: /home/user/RTR108/test1.py ====================
+2
+>>> 
+==================== RESTART: /home/user/RTR108/test1.py ====================
+5
+>>> 
+==================== RESTART: /home/user/RTR108/test1.py ====================
+>>> 
+==================== RESTART: /home/user/RTR108/test1.py ====================
+>>> 
+==================== RESTART: /home/user/RTR108/test1.py ====================
+>>> 
+==================== RESTART: /home/user/RTR108/test1.py ====================
+>>> print(x)
+Yahan par sara code likhna hai
+>>> 
+==================== RESTART: /home/user/RTR108/test1.py ====================
+Yahan par sara code likhna hai
+>>> 
+==================== RESTART: /home/user/RTR108/test1.py ====================
+>>> 
+============ RESTART: /home/user/RTR108/Conditional execution.py ============
+>>> 
+============ RESTART: /home/user/RTR108/Conditional execution.py ============
+>>> 5 == 5
+True
+>>> 
+============ RESTART: /home/user/RTR108/Conditional execution.py ============
+Traceback (most recent call last):
+  File "/home/user/RTR108/Conditional execution.py", line 1, in <module>
+    x <= y
+NameError: name 'x' is not defined
+>>> 
+============ RESTART: /home/user/RTR108/Conditional execution.py ============
+>>> 
+============ RESTART: /home/user/RTR108/Conditional execution.py ============
+>>> 
+============ RESTART: /home/user/RTR108/Conditional execution.py ============
+True
+False
+>>> 
+============ RESTART: /home/user/RTR108/Conditional execution.py ============
+True
+>>> 
+>>> 
+>>> 
+>>> 
+============ RESTART: /home/user/RTR108/Conditional execution.py ============
+True
+False
+>>> 
